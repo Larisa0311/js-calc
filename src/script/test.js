697d1308f6585d4bac04248908af5d1396978c29
Larisa0311 <zubenko0311@gmail.com>
2023-08-28 18:17:47 +0300

    init calculator

--- src/script/test.js
+++ src/script/test.js
@@ -1,1 +1,13 @@
 console.log('script')
+
+// const message = 'Default text'
+
+// const message2 = () => {}
+
+// function Message3() {}
+
+// const message3 = 'script'
+
+// export { message3, Message3, message2 }
+
+// export default message
